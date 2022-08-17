ce2e42c612bec0b5c3d748f7e38f0d279f14598f
TEKA fawez <97480903+TekaFawez@users.noreply.github.com>
2022-08-17 10:59:57 +0100

    axios

---
+++ frontend/src/component/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>ici about</div>
+  )
+}
+
+export default About
---
+++ frontend/src/component/topics.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Topics() {
+  return (
+    <div>ici ce topics</div>
+  )
+}
+
+export default Topics
